01bf2f6fb00d6031db7ac8cd68c39b79643e806f
Stig <stig@stig.io>
2018-02-03 19:46:10 +0100

    added orig and potree-processed point cloud data

---
+++ example/pointclouds/hackeriet/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 8890390,
+    "boundingBox": {
+        "lx": -16.366821,
+        "ly": -9.984478,
+        "lz": -0.066301,
+        "ux": 3.643909999999998,
+        "uy": 10.026253,
+        "uz": 19.94443
+    },
+    "tightBoundingBox": {
+        "lx": -16.366821,
+        "ly": -9.984478,
+        "lz": -0.066301,
+        "ux": 3.64391,
+        "uy": 7.865116,
+        "uz": 10.290956
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.17329801619052888,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
